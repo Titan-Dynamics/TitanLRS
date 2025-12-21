5daed48efce2a6665c736593eae16b7e1ccbebc7
wvarty <wezley.varty@gmail.com>
2025-12-21 22:36:47 +1000

    Dont spam backpack with extra CRSF telem

--- src/lib/tx-crsf/TXBackpackConnector.cpp
+++ src/lib/tx-crsf/TXBackpackConnector.cpp
@@ -1,4 +1,5 @@
 #include "TXBackpackConnector.h"
+#include "devBackpack.h"
 
 #include "config.h"
 
@@ -9,7 +10,16 @@ void TXBackpackConnector::forwardMessage(const crsf_header_t *message)
 {
     if (TxUSB != BackpackOrLogStrm)
     {
-        const uint8_t length = message->frame_size + CRSF_FRAME_NOT_COUNTED_BYTES;
-        BackpackOrLogStrm->write((uint8_t *)message, length);
+        // Only send LUA relevant messages to the backpack
+        if (message->type == CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY ||
+            message->type == CRSF_FRAMETYPE_PARAMETER_READ ||
+            message->type == CRSF_FRAMETYPE_PARAMETER_WRITE ||
+            message->type == CRSF_FRAMETYPE_DEVICE_INFO ||
+            message->type == CRSF_FRAMETYPE_DEVICE_PING ||
+            message->type == CRSF_FRAMETYPE_ELRS_STATUS)
+        {
+            const uint8_t length = message->frame_size + CRSF_FRAME_NOT_COUNTED_BYTES;
+            BackpackOrLogStrm->write((uint8_t *)message, length);
+        }
     }
 }
