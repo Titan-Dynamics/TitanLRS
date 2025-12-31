241805741d67e010665af31f4b142243f72c7711
wvarty <wezley.varty@gmail.com>
2025-12-31 12:30:24 +1000

    Fix for DIP modules

--- src/lib/tx-crsf/TXUSBConnector.cpp
+++ src/lib/tx-crsf/TXUSBConnector.cpp
@@ -12,4 +12,19 @@ void TXUSBConnector::forwardMessage(const crsf_header_t *message)
         const uint8_t length = message->frame_size + CRSF_FRAME_NOT_COUNTED_BYTES;
         TxUSB->write((uint8_t *)message, length);
     }
+
+    if (TxUSB == BackpackOrLogStrm)
+    {
+        // Only send LUA relevant messages to the backpack
+        if (message->type == CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY ||
+            message->type == CRSF_FRAMETYPE_PARAMETER_READ ||
+            message->type == CRSF_FRAMETYPE_PARAMETER_WRITE ||
+            message->type == CRSF_FRAMETYPE_DEVICE_INFO ||
+            message->type == CRSF_FRAMETYPE_DEVICE_PING ||
+            message->type == CRSF_FRAMETYPE_ELRS_STATUS)
+        {
+            const uint8_t length = message->frame_size + CRSF_FRAME_NOT_COUNTED_BYTES;
+            TxUSB->write((uint8_t *)message, length);
+        }
+    }
 }
